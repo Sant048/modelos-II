0505c7c29aea5e78f0e64d4f14bf4f174a45b65b
Your Name <you@example.com>
2025-03-31 07:10:09 -0500

    Generacion del proyecto en spring boot

---
+++ Manejo_end_points/api-productos/src/main/java/com/modelos/II/ud/distrital/api_productos/ApiProductosApplication.java
@@ -0,0 +1,13 @@
+package com.modelos.II.ud.distrital.api_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProductosApplication.class, args);
+	}
+
+}
---
+++ Manejo_end_points/api-productos/src/test/java/com/modelos/II/ud/distrital/api_productos/ApiProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelos.II.ud.distrital.api_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
